6d32b1fc15114f6f26b4d4c25d82ea207a0fcfeb
GabrieleSalati <gabriele.salati5@gmail.com>
2023-03-07 14:09:21 +0100

    Navbar added

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+    props: {
+        title: String,
+    },
+};
+</script>
+
+<template>
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand">{{ title }}</a>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search Movies and Series</button>
+            </form>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss" scoped></style>
