e44e2db22ddd2b16898f7448d5caf452ed43a47a
GabrieleSalati <gabriele.salati5@gmail.com>
2023-03-08 18:52:02 +0100

    Milestone 1 completed

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
 <script>
 import SearchBar from "./components/SearchBar.vue";
+import axios from 'axios';
 
 export default {
     data() {
         return {
-            title: "BoolFlix"
+            title: "BoolFlix",
+            movieList: [],
         }
     },
 
     components: {
         SearchBar
     },
+
+    methods: {
+        fetchMovies(query) {
+            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=14ae40f822e70b609bb2418462eb935d&query=${query}`)
+                .then((response) => {
+                    this.movieList = response.data.results;
+                })
+        },
+    },
 };
 </script>
 
 <template>
-    <SearchBar :title="title" />
+    <SearchBar :title="title" @search="fetchMovies" />
+
+    <ul>
+        <li v-for="movie in movieList">{{ movie.original_title }}</li>
+        <li v-for="movie in movieList">{{ movie.original_language }}</li>
+        <li v-for="movie in movieList">{{ movie.vote_average }}</li>
+    </ul>
 </template>
 
 <style lang="scss"></style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,9 +1,18 @@
 <script>
 
 export default {
+
+    data() {
+        return {
+            term: ''
+        }
+    },
+
     props: {
         title: String,
     },
+
+    emits: ['search'],
 };
 </script>
 
@@ -11,9 +20,10 @@ export default {
     <nav class="navbar bg-light">
         <div class="container-fluid">
             <a class="navbar-brand">{{ title }}</a>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search Movies and Series</button>
+            <form class="d-flex" role="search" @submit.prevent="$emit('search', term)">
+                <input v-model="term" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search Movies and
+                    Series</button>
             </form>
         </div>
     </nav>
